c50c0304448f0796b43b8ecd981a05a0582b4619
Lucas Anastasiou <anastluc@gmail.com>
2017-04-12 16:09:41 +0100

    adding db update calls

--- FrontiersHarvester/src/main/java/eu/openminted/toolkit/frontiers/harvester/FrontiersHarvesterService.java
+++ FrontiersHarvester/src/main/java/eu/openminted/toolkit/frontiers/harvester/FrontiersHarvesterService.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import eu.openminted.toolkit.crossref.CrossRefClient;
 import eu.openminted.toolkit.crossref.Utility;
 import eu.openminted.toolkit.crossref.model.multiple_works.Item;
+import eu.openminted.toolkit.database.exceptions.DatabaseException;
+import eu.openminted.toolkit.database.services.DoiDiscoveryLogServiceDAO;
 import eu.openminted.toolkit.queue.ScheduledArticle;
 import eu.openminted.toolkit.queue.services.QueueService;
 import java.util.List;
@@ -25,34 +27,45 @@ public class FrontiersHarvesterService implements CommandLineRunner {
     @Autowired
     QueueService queueService;
 
+    @Autowired
+    DoiDiscoveryLogServiceDAO doiDiscoveryLogServiceDAO;
+
     private static final Logger logger = Logger.getLogger("FrontiersHarvesterService");
 
     @Override
     public void run(String... strings) throws Exception {
 
+        queueService.declareDedicatedQueue("Frontiers-download-queue");
         this.scheduleMonthsPublications();
     }
 
     //
     // TODO make single-prefix able 
-    private void scheduleMonthsPublications() {
+    private void scheduleMonthsPublications() throws DatabaseException {
         for (String prefix : FrontiersConstants.FRONTIERS_PREFIXES) {
-            for (String month : this.getMonths()) {
-                List<Item> monthItems = crossRefClient.getPublisherMonthItems(prefix, month);
-                monthItems.forEach(item -> scheduleItem(prefix, item));
+            for (String date : this.getDays()) {
+                logger.info(String.format("Harvesting for prefix : %s for date : %s", prefix, date));
+                List<Item> dateRecords = crossRefClient.getPublisherItemsOfDate(prefix, date);
+                
+                long dbID = (int) doiDiscoveryLogServiceDAO.insertNewDiscovery("Frontiers", date);
+
+                dateRecords.forEach(item -> scheduleItem(prefix, item));
+
+                logger.info("DOIS of " + date + " (total " + dateRecords.size() + " records)");
+
+                doiDiscoveryLogServiceDAO.finishDoiDiscovery(dbID, "Frontiers");
+                doiDiscoveryLogServiceDAO.updateDiscoveredDoisCount(dbID, dateRecords.size());
 
             }
         }
     }
 
-    public void schedulePrefixMonthPublications(String prefix, String month) {
-        logger.info(String.format("Harvesting for prefix : %s for month : %s", prefix, month));
-        List<Item> monthItems = crossRefClient.getPublisherMonthItems(prefix, month);
-        monthItems.forEach(item -> scheduleItem(prefix, item));
+    private List<String> getMonths() {
+        return Utility.getMonthsAsStringFromYearToYear("2010", "2017");
     }
 
-    private List<String> getMonths() {
-        return Utility.getMonthsAsStringFromYearToYear("2007", "2009");
+    private List<String> getDays() {
+        return Utility.getDaysAsStringFromDateToDate("2010-10-01", "2017-04-12");
     }
 
     private void scheduleItem(String publisherPrefix, Item item) {
