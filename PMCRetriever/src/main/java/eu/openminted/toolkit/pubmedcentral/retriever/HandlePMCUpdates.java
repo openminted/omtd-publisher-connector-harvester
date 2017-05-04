c3aac389719b76411cd271a16db1d50077f5948e
Samuel Pearce <samuel.pearce@open.ac.uk>
2017-05-04 15:22:48 +0100

    Adds async processing of downloaded archive OMTD-69

--- PMCRetriever/src/main/java/eu/openminted/toolkit/pubmedcentral/retriever/HandlePMCUpdates.java
+++ PMCRetriever/src/main/java/eu/openminted/toolkit/pubmedcentral/retriever/HandlePMCUpdates.java
@@ -5,30 +5,22 @@
  */
 package eu.openminted.toolkit.pubmedcentral.retriever;
 
+import com.google.common.io.Files;
 import eu.openminted.dit.GenericArticleRetrieverService;
-import eu.openminted.pubmedcentral.api.RMetadata;
 import eu.openminted.pubmedcentral.api.ftp.PMCFtpClient;
 import eu.openminted.pubmedcentral.api.ftp.PMCFtpFile;
 import eu.openminted.pubmedcentral.api.saxparser.UpdateRecord;
 import eu.openminted.toolkit.database.exceptions.DatabaseException;
-import eu.openminted.toolkit.database.services.ArticleFilesDAO;
 import eu.openminted.toolkit.database.services.GenericArticleFileDAO;
 import eu.openminted.toolkit.pubmedcentral.retriever.Message.MessageEventCallback;
 import eu.openminted.toolkit.queue.ScheduledArticle;
 import eu.openminted.toolkit.storage.StorageDAO;
 import eu.openminted.toolkit.storage.exceptions.StorageException;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.zip.GZIPInputStream;
-import org.apache.commons.io.FileUtils;
-import org.apache.tools.tar.TarEntry;
-import org.apache.tools.tar.TarInputStream;
+import java.util.concurrent.ExecutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,18 +37,23 @@ public class HandlePMCUpdates implements MessageEventCallback {
 
     GenericArticleRetrieverService genericArticleRetrieverService;
 
+    ExecutorService executorService;
+
     Logger logger = LoggerFactory.getLogger("HandlePMCUpdates");
 
     @Autowired
-    public HandlePMCUpdates(StorageDAO storageDAO, GenericArticleFileDAO genericArticleFileDAO, GenericArticleRetrieverService genericArticleRetrieverService) {
+    public HandlePMCUpdates(StorageDAO storageDAO, GenericArticleFileDAO genericArticleFileDAO, GenericArticleRetrieverService genericArticleRetrieverService, ExecutorService executorService) {
         this.storageDAO = storageDAO;
         this.genericArticleFileDAO = genericArticleFileDAO;
         this.genericArticleRetrieverService = genericArticleRetrieverService;
+        this.executorService = executorService;
     }
 
     @Override
     public void callback(ScheduledArticle article) throws IOException, StorageException, DatabaseException {
         UpdateRecord updateRecord = UpdateRecord.newUpdateRecordFromGsonSerialisedString(article.getMetadata());
+
+        logger.info("Message Recieved " + updateRecord.getId());
 
         if (!"tgz".equals(updateRecord.getLinkFormat())) {
             logger.warn("Record item not a tar.gz file " + updateRecord.getId());
@@ -68,96 +65,19 @@ public class HandlePMCUpdates implements MessageEventCallback {
         final long startTime = System.currentTimeMillis();
 
         File temp = File.createTempFile(updateRecord.getId(), "." + updateRecord.getLinkFormat());
-        File tempPdf = null;
-        File tempXml = null;
+
         PMCFtpFile file = new PMCFtpFile(new PMCFtpClient(), url);
 
+        logger.info(updateRecord.getId() + " Downloading file Recieved " + url.toExternalForm());
         file.retrieve(new FileOutputStream(temp));
+        logger.info(updateRecord.getId() + " Finished Downloading file Recieved " + url.toExternalForm());
+
         final long endTime = System.currentTimeMillis();
 
-        logger.info(updateRecord.getId() + "Time to Download File: " + (endTime - startTime));
+        logger.info(updateRecord.getId() + " Time to Download File: " + (endTime - startTime));
 
-        Map<String, File> contents = new HashMap<>();
+        executorService.execute(new ProcessDownloadedDocument(temp, updateRecord, storageDAO, genericArticleFileDAO, genericArticleRetrieverService));        
+        logger.info(updateRecord.getId() + " Scheduled Processing");
 
-        FileInputStream fis = new FileInputStream(temp);
-        GZIPInputStream gis = new GZIPInputStream(fis);
-        TarInputStream tis = new TarInputStream(gis);
-        try {
-
-            TarEntry entry;
-            ByteArrayOutputStream baos;
-            while ((entry = tis.getNextEntry()) != null) {
-                String entryName = entry.getName();
-
-                if (entryName.endsWith("pdf") || entryName.endsWith("nxml")) {
-                    baos = new ByteArrayOutputStream();
-
-                    if (entryName.endsWith("pdf")) {
-                        tempPdf = File.createTempFile(updateRecord.getId(), ".pdf");
-                        tis.copyEntryContents(new FileOutputStream(tempPdf));
-                        contents.put("pdf", tempPdf);
-                    }
-
-                    if (entryName.endsWith("nxml")) {
-                        tempXml = File.createTempFile(updateRecord.getId(), ".xml");
-                        tis.copyEntryContents(new FileOutputStream(tempXml));
-                        contents.put("xml", tempXml);
-                    }
-                }
-            }
-
-            String publisher = "pubmed_central";
-            String pmcID = "";
-            if (contents.containsKey("xml")) {
-                File xmlFile = contents.get("xml");
-                try {
-
-                    RMetadata metadata = new RMetadata(xmlFile);
-
-                    publisher = metadata.publisher().isEmpty() ? "pubmed_central" : metadata.publisher().toLowerCase().trim().replace(" ", "_").replaceAll("\\W+", "");
-                    int maxLength = 50;
-                    if (publisher.length() > maxLength) {
-                        publisher = publisher.substring(0, maxLength);
-                    }
-                    pmcID = metadata.pmc();
-                    File fileLocation = new File(this.storageDAO.getMetadataFileLocation(publisher, pmcID, updateRecord.getLinkHref(), ".xml"));
-
-                    this.genericArticleFileDAO.insertNewArticle("pubmed_central", pmcID, "");
-                    this.genericArticleFileDAO.updateMetaFileLocation(pmcID, fileLocation.getAbsolutePath());
-
-                    FileUtils.copyFile(xmlFile, fileLocation);
-                } catch (Exception e) {
-                    logger.warn(e.getMessage(), e);
-                } finally {
-                    xmlFile.delete();
-                }
-
-            }
-            if (contents.containsKey("pdf")) {
-                File pdfFile = contents.get("pdf");
-                try {
-                    File fileLocation = new File(this.storageDAO.getPdfFileLocation(publisher, pmcID, updateRecord.getLinkHref()));
-
-                    FileUtils.copyFile(pdfFile, fileLocation);
-                    this.genericArticleFileDAO.updatePdfFileLocation(pmcID, fileLocation.getAbsolutePath());
-                } finally {
-                    pdfFile.delete();
-                }
-            }
-            final long endProcessTime = System.currentTimeMillis();
-
-            logger.info(updateRecord.getId() + "Time to Process File: " + (endProcessTime - endTime));
-        } finally {
-            temp.delete();
-            if (tempPdf != null) {
-                tempPdf.delete();
-            }
-            if (tempXml != null) {
-                tempXml.delete();
-            }
-            fis.close();
-            gis.close();
-            tis.close();
-        }
     }
 }
--- PMCRetriever/src/main/java/eu/openminted/toolkit/pubmedcentral/retriever/RetrieverConfiguration.java
+++ PMCRetriever/src/main/java/eu/openminted/toolkit/pubmedcentral/retriever/RetrieverConfiguration.java
@@ -6,6 +6,11 @@ import eu.openminted.dit.GenericArticleRetrieverService;
 import eu.openminted.toolkit.database.services.GenericArticleFileDAO;
 import eu.openminted.toolkit.pubmedcentral.retriever.Message.MessageEventCallback;
 import eu.openminted.toolkit.storage.StorageDAO;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PreDestroy;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -20,16 +25,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {"eu.openminted.toolkit"})
 class RetrieverConfiguration {
-
+    
     // Service not registered by componentscan
     @Bean
     GenericArticleRetrieverService genericArticleRetrieverService() {
-        return new GenericArticleRetrieverService();
+        return new GenericArticleRetrieverService();    
     }
     
     @Bean
-    MessageEventCallback messageCallback(StorageDAO storageDOA, GenericArticleFileDAO genericArticleFileDAO, GenericArticleRetrieverService genericArticleRetrieverService) {
-        return new HandlePMCUpdates(storageDOA, genericArticleFileDAO, genericArticleRetrieverService);
+    ExecutorService executorService() {
+        return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());    
+    }
+    
+    @Bean
+    ExecutorShutdownHook executorShutdown(ExecutorService executorService) {
+        return new ExecutorShutdownHook(executorService);
+    }
+    
+    
+    @Bean
+    MessageEventCallback messageCallback(StorageDAO storageDOA, GenericArticleFileDAO genericArticleFileDAO, GenericArticleRetrieverService genericArticleRetrieverService, ExecutorService executorService) {
+        return new HandlePMCUpdates(storageDOA, genericArticleFileDAO, genericArticleRetrieverService, executorService);
     }
     
     @Bean
@@ -46,11 +62,12 @@ class RetrieverConfiguration {
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-            MessageListenerAdapter listenerAdapter) {
+            MessageListenerAdapter listenerAdapter) {        
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
+        container.setPrefetchCount(50);
+        container.setMessageListener(listenerAdapter);        
         return container;
     }
 }
